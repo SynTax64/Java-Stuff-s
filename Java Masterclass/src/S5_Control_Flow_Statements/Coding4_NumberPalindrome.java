104615be92d8a0091193bf5563808f90274fcee4
David Aster <davidaster007@gmail.com>
2020-04-14 17:53:01 +0200

    Added "isPalindrom" coding exercise

--- Java Masterclass/src/S5_Control_Flow_Statements/Coding4_NumberPalindrome.java
+++ Java Masterclass/src/S5_Control_Flow_Statements/Coding4_NumberPalindrome.java
@@ -1,7 +1,24 @@
 package S5_Control_Flow_Statements;
 
 public class Coding4_NumberPalindrome {
+    public static void main(String[] args) {
+        isPalindrome(121);
+    }
+
     public static boolean isPalindrome(int number) {
-        
+        int origNumber = number;
+        int reverseNumber = 0;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+            number /= 10;
+        }
+        if (reverseNumber == origNumber) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
