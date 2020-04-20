fd565ea29cbd50c6dd5d2fccc629df268fd58749
David Aster <davidaster007@gmail.com>
2020-04-20 14:21:19 +0200

    Added Triangle class, modified now with corrected ExactlyOne class

--- JetBrains Academy/src/ExactlyOne.java
+++ JetBrains Academy/src/ExactlyOne.java
@@ -7,7 +7,9 @@ public class ExactlyOne {
         int number2 = scanner.nextInt();
         int number3 = scanner.nextInt();
 
-        if ((number1 > 0) ^ ((number2 > 0) ^ (number3 > 0))) {
+        if ((number1 > 0 && (number2 < 0 && number3 < 3)) ||
+                (number2 > 0 && (number1 < 0 && number3 < 0)) ||
+                (number3 > 0 && (number1 < 0 && number2 < 0))) {
             System.out.println(true);
         } else {
             System.out.println(false);
