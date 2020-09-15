17fd294b807c3c470ddd1777932fe31506543657
David Aster <davidaster007@gmail.com>
2020-09-15 16:40:55 +0200

    Added Reading numbers Challenge

---
+++ Java Masterclass/src/S5_Control_Flow_Statements/Challenge_ReadingUserInput.java
@@ -0,0 +1,31 @@
+package S5_Control_Flow_Statements;
+
+import java.util.Scanner;
+
+public class Challenge_ReadingUserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int counter = 0;
+        while (true) {
+            int order = counter + 1;
+            System.out.print("Enter number #" + order + ": ");
+
+            boolean isInt = scanner.hasNextInt();
+
+            if (isInt) {
+                int n = scanner.nextInt();
+                counter++;
+                sum += n;
+                if (counter == 10) {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid number!");
+            }
+            scanner.nextLine();
+
+        }
+        System.out.println("Sum of all numbers is: " + sum);
+    }
+}
