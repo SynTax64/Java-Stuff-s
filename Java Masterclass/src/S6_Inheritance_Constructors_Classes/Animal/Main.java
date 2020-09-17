9e66650ebd86e7ddc194eae6708aac342adf1352
David Aster <davidaster007@gmail.com>
2020-09-17 15:05:53 +0200

    Added Animal Package with (Animal, Main) classes

---
+++ Java Masterclass/src/S6_Inheritance_Constructors_Classes/Animal/Main.java
@@ -0,0 +1,9 @@
+package S6_Inheritance_Constructors_Classes.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Terrier", 60, 40, 4, 4, 20, 30, "silky woof");
+        dog.walk();
+        dog.run();
+    }
+}
