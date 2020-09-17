a84e93be741320f5dc77725476c2b3f1c4221477
David Aster <davidaster007@gmail.com>
2020-09-17 13:10:57 +0200

    Added ComplexNumber calculator

---
+++ Java Masterclass/src/S6_Inheritance_Constructors_Classes/ComplexNumber.java
@@ -0,0 +1,39 @@
+package S6_Inheritance_Constructors_Classes;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        this.imaginary += complexNumber.imaginary;
+        this.real += complexNumber.real;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.imaginary -= imaginary;
+        this.real -= real;
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        this.imaginary -= complexNumber.imaginary;
+        this.real -= complexNumber.real;
+    }
+}
