7d0315058b9f8a7d1abb4d6e8b3ebc08c7785f96
David Aster <davidaster007@gmail.com>
2020-09-21 12:36:40 +0200

    Renamed WallArea class

--- Java Masterclass/src/S6_Inheritance_Constructors_Classes/Coding20_WallArea.java
+++ Java Masterclass/src/S6_Inheritance_Constructors_Classes/Coding20_WallArea.java
@@ -1,14 +1,14 @@
 package S6_Inheritance_Constructors_Classes;
 
-public class Coding9_WallArea {
+public class Coding20_WallArea {
 
     private double width;
     private double height;
 
-    public Coding9_WallArea() {
+    public Coding20_WallArea() {
     }
 
-    public Coding9_WallArea(double width, double height) {
+    public Coding20_WallArea(double width, double height) {
         if (width < 0.0) {
             this.width = 0.0;
         } else {
@@ -51,7 +51,7 @@ public class Coding9_WallArea {
     }
 
     public static void main(String[] args) {
-        Coding9_WallArea wa = new Coding9_WallArea(1.125, -1.0);
+        Coding20_WallArea wa = new Coding20_WallArea(1.125, -1.0);
 
     }
 }
