8012efde424ca246d450f39b6e99cf88eecb10ca
David Aster <davidaster007@gmail.com>
2020-09-22 11:21:19 +0200

    Added Coding challenge Pool Area

---
+++ Java Masterclass/src/S6_Inheritance_Constructors_Classes/Codinf24_PoolArea/Cuboid.java
@@ -0,0 +1,22 @@
+package S6_Inheritance_Constructors_Classes.Codinf24_PoolArea;
+
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0.0) {
+            this.height = 0.0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height * getArea();
+    }
+}
