daebcf82da22b264e64cdbe28e17679ae1314598
David Aster <davidaster007@gmail.com>
2020-09-22 11:21:26 +0200

    Added Coding challenge Pool Area

---
+++ Java Masterclass/src/S6_Inheritance_Constructors_Classes/Codinf24_PoolArea/Rectangle.java
@@ -0,0 +1,31 @@
+package S6_Inheritance_Constructors_Classes.Codinf24_PoolArea;
+
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if (width < 0.0) {
+            this.width = 0.0;
+        } else {
+            this.width = width;
+        }
+        if (length < 0.0) {
+            this.length = 0.0;
+        } else {
+            this.length = length;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
