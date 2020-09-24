b65840d13640df8f4f0210d2eb77967bd744d274
David Aster <davidaster007@gmail.com>
2020-09-24 12:58:58 +0200

    Added Master challenge in S7 segment

---
+++ Java Masterclass/src/S7_Composition_Encapsulation_Polymorphism/MasterChallenge/DeluxeBurger.java
@@ -0,0 +1,29 @@
+package S7_Composition_Encapsulation_Polymorphism.MasterChallenge;
+
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        addHamburgerAddition1("Chips", 2.75);
+        addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+}
---
+++ Java Masterclass/src/S7_Composition_Encapsulation_Polymorphism/MasterChallenge/Hamburger.java
@@ -0,0 +1,93 @@
+package S7_Composition_Encapsulation_Polymorphism.MasterChallenge;
+
+public class Hamburger {
+
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+    private String addition1Name;
+    private double addition1Price;
+
+    private String addition2Name;
+    private double addition2Price;
+
+    private String addition3Name;
+    private double addition3Price;
+
+    private String addition4Name;
+    private double addition4Price;
+
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
+    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+        this.addition1Name = addition1Name;
+        this.addition1Price = addition1Price;
+    }
+
+    public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+        this.addition2Name = addition2Name;
+        this.addition2Price = addition2Price;
+    }
+
+    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+        this.addition3Name = addition3Name;
+        this.addition3Price = addition3Price;
+    }
+
+    public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+        this.addition4Name = addition4Name;
+        this.addition4Price = addition4Price;
+    }
+
+    public double itemizeHamburger() {
+        double hamburgerPrice = this.price;
+        System.out.println(this.name + " hamburger" + " on a " + this.breadRollType + " roll" + " price is " + this.price);
+        if (this.addition1Name != null) {
+            hamburgerPrice += addition1Price;
+            System.out.println("Added " + this.addition1Name + " for an extra " + this.addition1Price);
+        }
+
+        if (this.addition2Name != null) {
+            hamburgerPrice += addition2Price;
+            System.out.println("Added " + this.addition2Name + " for an extra " + this.addition2Price);
+        }
+
+        if (this.addition3Name != null) {
+            hamburgerPrice += addition3Price;
+            System.out.println("Added " + this.addition3Name + " for an extra " + this.addition3Price);
+        }
+
+        if (this.addition4Name != null) {
+            hamburgerPrice += addition4Price;
+            System.out.println("Added " + this.addition4Name + " for an extra " + this.addition4Price);
+        }
+
+        return hamburgerPrice;
+    }
+
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.50, "White");
+        hamburger.addHamburgerAddition1("Tomato", 0.25);
+        hamburger.addHamburgerAddition2("Carrot", 0.30);
+        hamburger.addHamburgerAddition3("Pickle", 0.21);
+        double price = hamburger.itemizeHamburger();
+        System.out.println("Total Burger price is " + price);
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.7);
+        healthyBurger.addHamburgerAddition1("Egg", 0.60);
+        healthyBurger.addHealthAddition1("Lentils", 0.80);
+        price = healthyBurger.itemizeHamburger();
+        System.out.println("Total Burger price is " + price);
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.itemizeHamburger();
+    }
+}
