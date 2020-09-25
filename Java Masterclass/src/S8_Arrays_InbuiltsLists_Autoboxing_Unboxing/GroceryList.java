ff630f8e1f3c3c7ea8c54935aaa92b01caa3d914
David Aster <davidaster007@gmail.com>
2020-09-25 14:17:55 +0200

    Added GroceryList general class and search item method to the Grocery list class

--- Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/GroceryList.java
+++ Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/GroceryList.java
@@ -6,30 +6,39 @@ public class GroceryList {
 
     ArrayList<String> groceryList = new ArrayList<String>();
 
-    private void printGroceryItems() {
+    public void printGroceryItems() {
         System.out.println("Your grocery list has: " + groceryList.size() + " items.");
-        int el = 1;
+        /*int el = 1;
         for (String item : groceryList) { // printed with foreach loop
             System.out.println("Item #" + el++ + ": " + item);
-        }
+        }*/
 
         for (int i = 0; i < groceryList.size(); i++) { // or with normal for loop
             System.out.println("Item #" + i + ": " + groceryList.get(i));
         }
     }
 
-    private void addGroceryItem(String item) {
+    public void addGroceryItem(String item) {
         groceryList.add(item); // how to add item in ArrayList
         System.out.println("It was added item: " + item);
     }
 
-    private void replaceGroceryItem(int position, String item) {
+    public void replaceGroceryItem(int position, String item) {
         groceryList.set(position, item); // how to replace item in ArrayList
         System.out.println("It was replaced item \"" + groceryList.get(position) + "\" on  the list");
     }
 
-    private void removeGroceryItem(int position) {
+    public void removeGroceryItem(int position) {
         String item = groceryList.get(position);
         groceryList.remove(position);
+    }
+
+    public String searchItem(String searchItem) {
+        boolean isExists = groceryList.contains(searchItem); // how to check if item exists in this list
+        int position = groceryList.indexOf(searchItem); // how to ge index of item, which we searching for, if not in list return -1
+        if (position >= 0) {
+            return groceryList.get(position); // if item exists return value of item
+        }
+        return null; // if not exists return empty object(null)
     }
 }
---
+++ Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/Main.java
@@ -0,0 +1,64 @@
+package S8_Arrays_InbuiltsLists_Autoboxing_Unboxing;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+    private static void printInstructions() {
+        System.out.println("0: Print instructions");
+        System.out.println("1: Print Grocery list");
+        System.out.println("2: Add item to the List");
+        System.out.println("3. Replace item from the list");
+        System.out.println("4. Remove item from the list");
+        System.out.println("5. Search item on the List");
+        System.out.println("6. To quit the application");
+    }
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while (!quit) {
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryItems();
+                    break;
+                case 2:
+                    System.out.println("Enter an item: ");
+                    String item = scanner.nextLine();
+                    groceryList.addGroceryItem(item);
+                    break;
+                case 3:
+                    System.out.print("Replace item on place (int) ");
+                    int place = scanner.nextInt() - 1;
+                    System.out.println("Enter an item: (String)");
+                    String cItem = scanner.nextLine();
+                    groceryList.replaceGroceryItem(place, cItem);
+                    break;
+                case 4:
+                    System.out.print("Enter place of item to be removed: ");
+                    groceryList.removeGroceryItem(scanner.nextInt());
+                    break;
+                case 5:
+                    System.out.print("Enter name of the item: ");
+                    String sItem = groceryList.searchItem(scanner.nextLine());
+                    if (sItem != null) {
+                        System.out.println("Item " + sItem + " found");
+                    } else {
+                        System.out.println("Item " + sItem + " not found");
+                    }
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+}
