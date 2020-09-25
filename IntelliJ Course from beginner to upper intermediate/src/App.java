844470531a36767bb5c5d24f65a9b929980af4a5
David Aster <davidaster007@gmail.com>
2020-09-25 11:07:49 +0200

    Removed not necessary packages

--- IntelliJ Course from beginner to upper intermediate/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        String string = "Heute";
-        System.out.println("Something");
-    }
-}
--- IntelliJ Course from beginner to upper intermediate/src/Debugging/SimpleDebug.java
+++
@@ -1,8 +0,0 @@
-package Debugging;
-
-public class SimpleDebug {
-    public static void main(String[] args) {
-        String name = "David";
-        System.out.println("Hello " + name);
-    }
-}
