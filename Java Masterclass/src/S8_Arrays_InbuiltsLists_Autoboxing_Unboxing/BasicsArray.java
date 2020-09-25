59ed57f14c40fc8d4c3067f85d91123a9d0b5621
David Aster <davidaster007@gmail.com>
2020-09-25 10:18:41 +0200

    Fixed access to the methods

--- Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/BasicsArray.java
+++ Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/BasicsArray.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BasicsArray {
-    public static void printArray(int[] array) {
+    private static void printArray(int[] array) {
         int count = 0;
         for (int element : array) {
             if (count % 20 == 0 && count > 0) {
@@ -15,7 +15,7 @@ public class BasicsArray {
         }
     }
 
-    public static int[] createArray(int size) {
+    private static int[] createArray(int size) {
         int[] arrayOfInts = new int[size];
         for (int i = 0; i < arrayOfInts.length; i++) {
             arrayOfInts[i] = new Random().nextInt(100);
@@ -23,12 +23,12 @@ public class BasicsArray {
         return arrayOfInts;
     }
 
-    public static double getAverage(int[] array) {
+    private static double getAverage(int[] array) {
         int sumOfArray = 0;
         for (int element : array) {
             sumOfArray += element;
         }
-        return sumOfArray / array.length;
+        return (double) sumOfArray / array.length;
     }
 
     public static void main(String[] args) {
--- Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/Challenge12_SortingArray.java
+++ Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/Challenge12_SortingArray.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Challenge12_SortingArray {
 
-    public static int[] sortArray(int[] array) {
+    private static int[] sortArray(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
@@ -17,7 +17,7 @@ public class Challenge12_SortingArray {
         }
         return array;
     }
-    public static int[] createArray(int size) {
+    private static int[] createArray(int size) {
         int[] arrayOfInts = new int[size];
         for (int i = 0; i < arrayOfInts.length; i++) {
             arrayOfInts[i] = new Random().nextInt(100);
@@ -25,7 +25,7 @@ public class Challenge12_SortingArray {
         return arrayOfInts;
     }
 
-    public static void printArray(int[] array) {
+    private static void printArray(int[] array) {
         int count = 0;
         for (int element : array) {
             if (count % 20 == 0 && count > 0) {
