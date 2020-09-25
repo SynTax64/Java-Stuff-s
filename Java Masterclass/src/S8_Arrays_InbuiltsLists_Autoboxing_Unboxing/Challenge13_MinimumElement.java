fc0fdb7886ea0d28cde5dfa3d22e9c6e9ff3616f
David Aster <davidaster007@gmail.com>
2020-09-25 09:31:03 +0200

    Added some text by the output in challenge13

--- Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/Challenge13_MinimumElement.java
+++ Java Masterclass/src/S8_Arrays_InbuiltsLists_Autoboxing_Unboxing/Challenge13_MinimumElement.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Challenge13_MinimumElement {
     public static Scanner scanner = new Scanner(System.in);
 
-    public static int[] readIntegers(int count) {
+    private static int[] readIntegers(int count) {
         int[] arrayOfInts = new int[count];
         for (int i = 0; i < arrayOfInts.length; i++) {
+            System.out.printf("Enter the #%d number: ", (i + 1));
             arrayOfInts[i] = scanner.nextInt();
         }
         return arrayOfInts;
     }
 
-    public static int findMin(int[] array) {
+    private static int findMin(int[] array) {
         int minValue = Integer.MAX_VALUE;
 
         for (int i = 0; i < array.length; i++) {
@@ -24,7 +25,7 @@ public class Challenge13_MinimumElement {
         return minValue;
     }
 
-    public static void printArray(int[] array) {
+    private static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
