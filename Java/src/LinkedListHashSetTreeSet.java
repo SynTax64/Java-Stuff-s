2b75472d75cc050cb9b8a83d7ec3c0e7cb5f563b
David Aster <davidaster007@gmail.com>
2020-10-01 10:19:50 +0200

    Added LinkedList HashSet, TreeSet collections

---
+++ Java/src/LinkedListHashSetTreeSet.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class LinkedListHashSetTreeSet {
+    public static String printCollections(Collection<Integer> collection) {
+        String str = "";
+        for (Integer el : collection) {
+            str += el + " ";
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        TreeSet<Integer> treeSet = new TreeSet<Integer>();
+
+        for (int i = 0; i < 50; i++) {
+            linkedList.add((int) new Random().nextInt(10));
+            hashSet.add((int) new Random().nextInt(10));
+            treeSet.add((int) new Random().nextInt(10));
+        }
+
+        System.out.println("LinkedList: " + printCollections(linkedList));
+        System.out.println("HashSet: " + printCollections(hashSet));
+        System.out.println("TreeSet: " + printCollections(treeSet));
+
+
+    }
+}
