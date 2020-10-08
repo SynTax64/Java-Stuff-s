c2cce82bfa93ac2f32d931b5429ad93ecb0f517b
David Aster <davidaster007@gmail.com>
2020-10-08 10:18:31 +0200

    Added AWT Library to the Java Masterclass section 10

---
+++ Java Masterclass/src/S11_Packages_Static_And_Final/Main.java
@@ -0,0 +1,9 @@
+package S11_Packages_Static_And_Final;
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("This is AWT Frame");
+        myWindow.setVisible(true);
+
+    }
+}
---
+++ Java Masterclass/src/S11_Packages_Static_And_Final/PackageExample.java
@@ -0,0 +1,9 @@
+package S11_Packages_Static_And_Final;
+
+public class PackageExample {
+    public static void main(String[] args) {
+        org.w3c.dom.Node node = null;
+
+    }
+
+}
