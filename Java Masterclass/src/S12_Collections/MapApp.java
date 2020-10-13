d0073bbfd58a589136fc6adca10053f8b139ed17
David Aster <davidaster007@gmail.com>
2020-10-13 14:44:18 +0200

    Added Location Class, MapApp test class for testing the mentioned class (working with Map(HashMap)), added picture of places

--- Java Masterclass/src/S12_Collections/MapApp.java
+++ Java Masterclass/src/S12_Collections/MapApp.java
@@ -1,28 +1,91 @@
 package S12_Collections;
 
-import java.io.StringReader;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class MapApp {
+
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
     public static void main(String[] args) {
-        Map<String, String> languages = new HashMap<>();
-        languages.put("Java", "Compile High level Object oriented language");
-        languages.put("Python", "Interpreted object oriented high level programming language with dynamic semantics");
-        languages.put("Algol", "an algorithmic language");
-        languages.put("BASIC", "Beginners all purposes symbolic instruction code");
+//        Map<String, String> languages = new HashMap<>();
+//        languages.put("Java", "Compile High level Object oriented language");
+//        languages.put("Python", "Interpreted object oriented high level programming language with dynamic semantics");
+//        languages.put("Algol", "an algorithmic language");
+//        languages.put("BASIC", "Beginners all purposes symbolic instruction code");
+//
+//        /*System.out.println(languages.get("Algol"));
+//        System.out.println(languages.get("Java"));*/
+//        if (languages.containsKey("Java")) {
+//            System.out.println("Java is already in this map");
+//        } else {
+//            languages.put("Java", "is course is about Java");
+//        }
+//        System.out.println(languages.get("Java"));
+//
+//        for (String key : languages.keySet()) {
+//            System.out.println(key + " key: " + languages.get(key));
+//        }
 
-        /*System.out.println(languages.get("Algol"));
-        System.out.println(languages.get("Java"));*/
-        if (languages.containsKey("Java")) {
-            System.out.println("Java is already in this map");
-        } else {
-            languages.put("Java", "is course is about Java");
+        Scanner scanner = new Scanner(System.in);
+        locations.put(0, new Location(0, "You are siting in front of a computer learning Java"));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2, "You are at the top of a hill"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+//        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
+
+        int loc = 1;
+        while (true) {
+            System.out.println(locations.get(loc).getDescription());
+            if (loc == 0) {
+                break;
+            }
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + " ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
         }
-        System.out.println(languages.get("Java"));
 
-        for (String key : languages.keySet()) {
-            System.out.println(key + " key: " + languages.get(key));
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for (String word : road) {
+            System.out.println(word);
+        }
+        String[] building = "You are inside a building, a well house for a small spring".split(",");
+        for (String word : building) {
+            System.out.println(word);
         }
     }
 }
