3b900d6910942e38a5323b4e85f70bf9b74acb10
David Aster <davidaster007@gmail.com>
2020-10-22 15:19:57 +0200

    Added Flow Pane layout

---
+++ Java Masterclass/src/S13_JavaFX/FlowPane/module-info.java
@@ -0,0 +1,7 @@
+module FlowPane {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens sample;
+
+}
---
+++ Java Masterclass/src/S13_JavaFX/FlowPane/sample/Controller.java
@@ -0,0 +1,4 @@
+package S13_JavaFX.FlowPane.sample;
+
+public class Controller {
+}
