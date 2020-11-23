24f014cebd447d994602b86cc7075eccda5cf33b
David Aster <davidaster007@gmail.com>
2020-11-23 13:58:15 +0100

    Implemented Thread.join() and interrupt() methods

--- Java Masterclass/src/S15_Concurrency/AnotherThread.java
+++ Java Masterclass/src/S15_Concurrency/AnotherThread.java
@@ -5,7 +5,7 @@ public class AnotherThread extends Thread {
     public void run() {
         System.out.println(ThreadColor.ANSI_BLUE + "Hello from " + currentThread().getName());
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             System.out.println(ThreadColor.ANSI_BLUE + "Another thread woke up");
         }
--- Java Masterclass/src/S15_Concurrency/Threads.java
+++ Java Masterclass/src/S15_Concurrency/Threads.java
@@ -19,9 +19,17 @@ public class Threads {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class´s implementation of run()");
+                try {
+                    anotherThread.join(2000);
+                    System.out.println(ANSI_RED + "anotherThread terminated, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
             }
         });
         myRunnableThread.start();
+//        anotherThread.interrupt();
+
         System.out.println(ANSI_PURPLE + "Hello again from the main thread");
     }
 }
