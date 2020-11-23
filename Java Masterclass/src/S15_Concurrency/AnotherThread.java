6e037d57175dc2f7f99033efbfd80ca48e777eb4
David Aster <davidaster007@gmail.com>
2020-11-23 13:45:40 +0100

    Implemented Thread.sleep() method

--- Java Masterclass/src/S15_Concurrency/AnotherThread.java
+++ Java Masterclass/src/S15_Concurrency/AnotherThread.java
@@ -3,6 +3,13 @@ package S15_Concurrency;
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println(ThreadColor.ANSI_BLUE + "This is another thread");
+        System.out.println(ThreadColor.ANSI_BLUE + "Hello from " + currentThread().getName());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(ThreadColor.ANSI_BLUE + "Another thread woke up");
+        }
+
+        System.out.println("Three seconds have passed and I'm awake");
     }
 }
--- Java Masterclass/src/S15_Concurrency/Threads.java
+++ Java Masterclass/src/S15_Concurrency/Threads.java
@@ -7,6 +7,7 @@ public class Threads {
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
         new Thread() {
             public void run() {
@@ -17,7 +18,7 @@ public class Threads {
         Thread myRunnableThread = new Thread(new MyRunnable() {
             @Override
             public void run() {
-                System.out.println(ANSI_RED+"Hello from the anonymous class´s implementation of run()");
+                System.out.println(ANSI_RED + "Hello from the anonymous class´s implementation of run()");
             }
         });
         myRunnableThread.start();
