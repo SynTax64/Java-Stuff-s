29ea354ff87d7ae7172c965c84ed2edddaaf36aa
David A <davidaster007@gmail.com>
2020-12-17 16:54:23 +0100

    Added Runnable to the Lambda expression

--- Java Masterclass/src/S16_LambdaExpressions/Main.java
+++ Java Masterclass/src/S16_LambdaExpressions/Main.java
@@ -1,4 +1,21 @@
 package S16_LambdaExpressions;
 
 public class Main {
+    public static void main(String[] args) {
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the Runnable");
+//            }
+//        }).start();
+        new Thread(() -> System.out.println("Printing from the Runnable")).start();
+    }
+}
+
+class CodeToRun implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Printing from the Runnable");
+    }
 }
