c73f37d5a79db59689e8334aa0646eded1aa5d27
David A <davidaster007@gmail.com>
2020-12-18 14:15:15 +0100

    Added comparator to compare object trough getName() method

--- Java Masterclass/src/S16_LambdaExpressions/Main.java
+++ Java Masterclass/src/S16_LambdaExpressions/Main.java
@@ -1,5 +1,10 @@
 package S16_LambdaExpressions;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
 //        new Thread(new Runnable() {
@@ -8,14 +13,56 @@ public class Main {
 //                System.out.println("Printing from the Runnable");
 //            }
 //        }).start();
-        new Thread(() -> System.out.println("Printing from the Runnable")).start();
+        new Thread(() -> {
+            System.out.println("Printing from the Runnable");
+            System.out.println("Line 2");
+            System.out.printf("This is line %d\n", 3);
+        }).start();
+        Employee john = new Employee("John", 37);
+        Employee david = new Employee("David", 34);
+        Employee tim = new Employee("Tim", 39);
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(david);
+        employees.add(tim);
+
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        });
+
+        for (Employee employee : employees) {
+            System.out.println(employee.getName());
+        }
     }
 }
 
-class CodeToRun implements Runnable {
 
-    @Override
-    public void run() {
-        System.out.println("Printing from the Runnable");
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
