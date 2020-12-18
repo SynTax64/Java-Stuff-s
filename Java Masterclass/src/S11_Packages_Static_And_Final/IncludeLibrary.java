05d6f35980b3bc4519c4e9b6f7377793f80844c4
David A <davidaster007@gmail.com>
2020-12-18 14:01:35 +0100

    Commented some FX code

--- Java Masterclass/src/S11_Packages_Static_And_Final/IncludeLibrary.java
+++ Java Masterclass/src/S11_Packages_Static_And_Final/IncludeLibrary.java
@@ -1,24 +1,24 @@
-package S11_Packages_Static_And_Final;
-
-
-import com.challenge.Series;
-
-import java.util.Random;
-
-public class IncludeLibrary {
-    public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print(Series.nSum(i) + " ");
-        }
-        System.out.println();
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print(Series.factorial(i) + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < 10; i++) {
-            System.out.print(Series.fibonacci(i) + " ");
-        }
-    }
-}
+//package S11_Packages_Static_And_Final;
+//
+//
+//import com.challenge.Series;
+//
+//import java.util.Random;
+//
+//public class IncludeLibrary {
+//    public static void main(String[] args) {
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.print(Series.nSum(i) + " ");
+//        }
+//        System.out.println();
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.print(Series.factorial(i) + " ");
+//        }
+//        System.out.println();
+//        for (int i = 0; i < 10; i++) {
+//            System.out.print(Series.fibonacci(i) + " ");
+//        }
+//    }
+//}
