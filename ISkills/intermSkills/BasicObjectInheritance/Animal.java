5138f25a55cbf54d30ac694fbda0d4578bf03c80
David A. <davidaster007@gmail.com>
2021-03-10 16:06:22 +0100

    added and used the inheritance

--- ISkills/intermSkills/BasicObjectInheritance/Animal.java
+++ ISkills/intermSkills/BasicObjectInheritance/Animal.java
@@ -3,18 +3,36 @@ package intermSkills.BasicObjectInheritance;
 public class Animal {
 
 	private String name;
+	private String breed;
 
 	public Animal() {
 
 	}
 
-	public Animal(String name) {
+	public Animal(String name, String breed) {
 		this.name = name;
+		this.breed = breed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
 	}
 
 	@Override
 	public String toString() {
 
-		return String.format("%s] Name: %s\n", getClass().getName(), this.name);
+		return String.format("%s] Breed: %s\tName: %s\n", getClass().getName(), getBreed(), getName());
 	}
 }
---
+++ ISkills/intermSkills/BasicObjectInheritance/Cat.java
@@ -0,0 +1,27 @@
+package intermSkills.BasicObjectInheritance;
+
+public class Cat extends Animal {
+	private boolean isDeclawed;
+
+	public Cat() {
+		super();
+	}
+
+	public Cat(String name, String breed, boolean isDeclawed) {
+		super(name, breed);
+		this.isDeclawed = isDeclawed;
+	}
+
+	public boolean isDeclawed() {
+		return isDeclawed;
+	}
+
+	public void setDeclawed(boolean isDeclawed) {
+		this.isDeclawed = isDeclawed;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s\tIs declawed: %b\n", super.toString(), isDeclawed);
+	}
+}
